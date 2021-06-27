390e876f3da1d7967dcdbfc3dece8eb05d4a7b3a
Chetan Bhat <chetansb77@gmail.com>
2021-06-28 00:48:27 +0530

    Crud Operations Implemented

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.blog import schemas as blogschemas
+from app.blog import models
+from app.db import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post('/create-blog')
-def create_blog(blog: blogschemas.Blog):
+def create_blog(blog: blogschemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, description=blog.description,
+                           body=blog.body, published=blog.published)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blogs')
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def get_blob_by_id(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     return blog
