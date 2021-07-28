198048e0bd21cb4e3100390d4b764a1b55ce4a4c
Chetan Bhat <chetansb77@gmail.com>
2021-07-29 01:48:30 +0530

    Authentication added

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from os import name
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from app.blog import schemas as blogschemas
 from app.authentication import schemas as authschemas
+from app.authentication.controller import Authentication
 from app import models
 from app.db import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
 
-app = FastAPI()
+app = FastAPI(
+    title="Fast API template project",
+    description="Template Project for reference and quick start",
+    version="1.0.0"
+    )
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,7 +24,7 @@ def get_db():
         db.close()
 
 
-@app.post('/create-blog', status_code=status.HTTP_201_CREATED)
+@app.post('/create-blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create_blog(blog: blogschemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.blogmodels.Blog(title=blog.title, description=blog.description,
                                         body=blog.body, published=blog.published)
@@ -29,13 +34,13 @@ def create_blog(blog: blogschemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blogs', status_code=status.HTTP_200_OK, response_model=List[blogschemas.ShowBlog])
+@app.get('/blogs', status_code=status.HTTP_200_OK, response_model=List[blogschemas.ShowBlog], tags=['blogs'])
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.blogmodels.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=blogschemas.ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=blogschemas.ShowBlog, tags=['blogs'])
 def get_blob_by_id(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.blogmodels.Blog).filter(models.blogmodels.Blog.id == id).first()
     if not blog:
@@ -43,7 +48,7 @@ def get_blob_by_id(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def destroy_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.blogmodels.Blog).filter(models.blogmodels.Blog.id == id)
     if not blog.first():
@@ -53,7 +58,7 @@ def destroy_blog(id, db: Session = Depends(get_db)):
     return {"Details": "Deleted"}
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update_blog(id, blog: blogschemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.blogmodels.Blog).filter(models.blogmodels.Blog.id == id)
     if not blog.first():
@@ -63,10 +68,19 @@ def update_blog(id, blog: blogschemas.Blog, db: Session = Depends(get_db)):
     return "Updated"
 
 
-@app.post('/user')
+@app.post('/user', response_model=authschemas.ShowUser, tags=['users'])
 def create_user(user: authschemas.User, db: Session = Depends(get_db)):
-    new_user = models.authmodels.User(name=user.name, email=user.email, password=user.password)
+    new_user = models.authmodels.User(name=user.name, email=user.email, \
+                password=Authentication.get_password_hash(user.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/getall', response_model=List[authschemas.ShowUser], tags=['users'])
+def get_all_user(db: Session = Depends(get_db)):
+    users = db.query(models.authmodels.User).all()  
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No users available")
+    return users
